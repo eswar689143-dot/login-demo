d3f6a21d2496d261c6eae35ff61c9e83ff423583
Eswarkondababu <eswar689143@gmail.com>
2025-12-12 23:18:27 +0530

    updated

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,8 +5,5 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100))
     email = Column(String(255), unique=True, index=True)
-    password = Column(String(255))
-
-
+    hashed_password = Column(String(255))  # MUST match backend
