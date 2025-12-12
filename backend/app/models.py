86a5d159efa9ec3a3ee92f8055a890571f4dac3e
Eswarkondababu <eswar689143@gmail.com>
2025-12-12 18:22:20 +0530

    update all the possibilities

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, Integer, String
 from .database import Base
 
-
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    name = Column(String(100))
+    email = Column(String(255), unique=True, index=True)
+    password = Column(String(255))
